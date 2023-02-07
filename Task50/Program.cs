013875eecb33973fb206b1883257877d8d279603
Aleksandr <zaitsey@yandex.ru>
2023-02-07 12:31:13 +0300

    Task50

---
+++ Task50/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.*/
+
+System.Console.WriteLine("Enter the number of rows");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter the number of columns");
+int b = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[10, 10];
+FillArrayRandomNumbers(numbers);
+
+if (a > numbers.GetLength(0) || b > numbers.GetLength (1))
+{
+    System.Console.WriteLine("There is no such element");
+} 
+else 
+{
+    System.Console.WriteLine($" the value of the {a} row and {b} column element is {numbers[a-1,b-1]}");
+
+}
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.Write("]");
+        Console.WriteLine(""); 
+    }
+}
